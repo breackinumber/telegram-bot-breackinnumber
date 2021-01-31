32b18611ce73fb7a24ca7fe010c015a7b0bca14e
Artur <artursemenenko2000@gmail.com>
2021-01-31 14:14:22 +0200

    Class "Person" has been done & tested.

--- src/com/breackinnumber/Main.java
+++ src/com/breackinnumber/Main.java
@@ -2,8 +2,20 @@ package com.breackinnumber;
 
 public class Main {
     public static void main(String[] args){
- System.out.print("Fuck off");
-
-
+        Person artur = new Person();
+        {artur.name = "Artur";
+        artur.age = 16;
+        artur.weight = 66.2;}
+        Person pedro = new Person();
+        {pedro.name = "Pedro";
+        pedro.age = 28;
+        pedro.weight = 92.6;}
+        printPersonCharacteristics(artur);
+        printPersonCharacteristics(pedro);
+    }
+    public static void  printPersonCharacteristics(Person person){
+        System.out.println("Person name is "+person.name);
+        System.out.println(person.name+" is "+person.age+" years old");
+        System.out.println(person.name+"'s weight is "+person.weight+" kilograms");
     }
 }
---
+++ src/com/breackinnumber/Person.java
@@ -0,0 +1,7 @@
+package com.breackinnumber;
+
+public class Person {
+    String name;
+    int age;
+    double weight;
+}
